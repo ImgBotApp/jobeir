d9ad6ab760701f5df8de1bfbaa743caca5345a23
brotzky <brotzky@gmail.com>
2017-03-02 21:06:09 -0800

    Adding Avenir uris, api, and login

--- client/modules/login/sagas/index.js
+++ client/modules/login/sagas/index.js
@@ -1,31 +1,19 @@
 import { put, call, takeEvery } from 'redux-saga/effects';
 import docCookies from '../../../utils/cookies';
+import { fetchApi } from '../../../utils/api';
 import {
   LOGIN_REQUESTED,
   LOGIN_SUCCEEDED,
   LOGIN_FAILED,
 } from '../ducks/';
 
-const headers = {
-  'Accept': 'application/json',
-  'Content-Type': 'application/json'
-};
-
-const loginCall = (payload) => {
-  const body = JSON.stringify(payload);
-
-  return fetch('/api/v0/login', {
-    method: 'POST',
-    headers,
-    body
-  })
-  .then(res => res.json());
-};
-
 export function* login(action) {
    try {
-      const payload = yield call(loginCall, action.payload);
-      docCookies.setItem('SID', payload.data.token)
+      const loginApi = fetchApi('POST', '/login', action.payload);
+      const payload = yield call(loginApi);
+
+      docCookies.setItem('SID', payload.data.token);
+      
       yield put({
         type: LOGIN_SUCCEEDED, 
         payload,
--- client/utils/api.js
+++ client/utils/api.js
@@ -1,8 +1,16 @@
 import docCookies from './cookies';
-export const baseUrl = '/api/0';
 
-export const reqHeaders = () => {
-  const headers = {};
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
+
+function reqHeaders() {
   const SID = docCookies.getItem('SID');
 
   const headers = {
@@ -15,4 +23,18 @@ export const reqHeaders = () => {
   }
 
   return headers;
+};
+
+export function fetchApi(method, endpoint, payload) {
+  console.log({method, endpoint, payload});
+  const url = `/api/v0/${endpoint}`;
+  const options = {
+    method,
+    headers: reqHeaders(),
+    body: JSON.stringify(payload)
+  };
+
+  return fetch(url, options)
+    .then(checkStatus)
+    .then(res => res.json());
 };
