8ff409bb9ae68a9c7f8cc254184cc22fc3bc3379
brotzky <brotzky@gmail.com>
2017-03-11 17:37:28 -0800

    Adding tests to modal actions and renaming test files

---
+++ client/modules/modal/ducks/modal.ducks.test.js
@@ -0,0 +1,25 @@
+import {
+  HIDE_MODAL,
+  SHOW_MODAL,
+  hideModal,
+  showModal,
+} from './';
+
+const action = {
+  type: SHOW_MODAL,
+  modalType: 'EXAMPLE_MODAL',
+  modalProps: 'dispatch',
+};
+
+describe('Modal', () => {
+  describe('showModal', () => {
+    it('should return the correct modal type and props', () => {
+      expect(showModal('EXAMPLE_MODAL', 'dispatch')).toEqual(action);
+    });
+  }); 
+  describe('hideModal', () => {
+    it('should return the type HIDE_MODAL', () => {
+      expect(hideModal()).toEqual({ type: HIDE_MODAL });
+    });
+  }); 
+});
