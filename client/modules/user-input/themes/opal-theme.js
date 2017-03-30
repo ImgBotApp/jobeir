1aa2a04f494ec99c7c1f7957c0faf7b48f3bb58e
brotzky <brotzky@gmail.com>
2017-03-29 23:53:02 -0700

    Adding in more verbose opal theme

--- client/modules/user-input/themes/opal-theme.js
+++ client/modules/user-input/themes/opal-theme.js
@@ -1,3 +1,25 @@
 export const opal = {
-  main: 'opal',
+  input: {
+    borderRadius: '',
+    border: '',
+    padding: '',
+    fontSize: '',
+    width: '',
+    margin: '',
+    activeBorderColor: '',
+    ph: {
+      color: '',
+    },
+  },
+  button: {
+    height: '',
+    borderRadius: '',
+    outline: '',
+    border: '',
+    background: '',
+    width: '',
+    marginBottom: '',
+    fontSize: '',
+    color: '',
+  }
 };
