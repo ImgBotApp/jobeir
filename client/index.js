405acaf24c5a517bbb924ef99d881ab645d905f6
brotzky <brotzky@gmail.com>
2017-09-22 23:01:44 -0700

    Back to the original client file

--- client/index.js
+++ client/index.js
@@ -3,31 +3,41 @@
  */
 import React from 'react';
 import { render } from 'react-dom';
-import { browserHistory } from 'react-router';
+import { match, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 import { AppContainer } from 'react-hot-loader';
 import configureStore from './redux/store';
 import routes from './routes';
 import initServiceWorker from './sw';
 import App from './modules/app/containers/App';
 
+// Mount into #app
+const mountApp = document.getElementById('app');
+
 // Initialize Servicie Worker register code
 // initServiceWorker();
 
 // Initialize store
 const store = configureStore(browserHistory, window.__INITIAL_STATE__);
+const history = syncHistoryWithStore(browserHistory, store);
 
-const renderApp = () => {
+const renderApp = renderProps =>
   render(
     <AppContainer>
-      <App store={store} />
+      <App {...{ store, history, ...renderProps }} />
     </AppContainer>,
-    document.getElementById('app')
+    mountApp
   );
-};
+
+match({ history, routes }, (error, redirectLocation, renderProps) =>
+  renderApp(renderProps)
+);
 
 // For hot reloading of react components
 if (module.hot) {
-  module.hot.accept('./modules/app/containers/App', renderApp());
-}
+  module.hot.accept('./routes', () => {
+    const nextRoutes = require('./routes'); // eslint-disable-line global-require
 
-renderApp();
+    renderApp({ routes: nextRoutes });
+  });
+}
